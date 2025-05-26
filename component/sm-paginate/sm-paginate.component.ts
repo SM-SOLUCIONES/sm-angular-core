9d02b23de1f597f1248ac0e58a67592d9d47befa
TaielMG <taielmartinezgort@gmail.com>
2025-05-26 15:46:23 -0300

    Sync

--- component/sm-paginate/sm-paginate.component.ts
+++ component/sm-paginate/sm-paginate.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgSelectComponent } from '@ng-select/ng-select';
@@ -6,7 +12,6 @@ import { PaginateConfigModel } from '../../models/PaginateConfig.model';
 import { FunctionService } from '../../services/function.services';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'sm-paginate',
   standalone: true,
@@ -19,12 +24,11 @@ export class SmPaginateComponent {
   @Input() paginate: PaginateConfigModel = new PaginateConfigModel();
   @Output() setPageEvent: EventEmitter<any> = new EventEmitter();
   itemsSize: any[] = [{ size: 10 }, { size: 50 }, { size: 100 }, { size: 250 }];
-
   constructor(
+    private cd: ChangeDetectorRef,
     public router: Router,
     private functionService: FunctionService
   ) {}
-
   getItemsPaginasSizes(): { size: number }[] {
     var items: { size: number }[] = [];
     this.paginate.itemsPaginasSizes.forEach((size) =>
@@ -34,21 +38,16 @@ export class SmPaginateComponent {
   }
 
   setPage(page: number) {
-    console.log('setPage |', 'page:', page, 'paginada:', this.paginate);
-
     this.paginate.page = page;
     this.setPageEvent.emit({
-      page: this.functionService.getNumber(this.paginate.page, 0),
+      page: this.functionService.getNumber(this.paginate.page, 1),
       size: this.functionService.getNumber(this.paginate.size, 10),
     });
   }
 
   setSize(event: any) {
-    console.log('setSize |', 'event:', event, 'paginada:', this.paginate);
-
-    this.paginate.page = 0;
     this.setPageEvent.emit({
-      page: this.functionService.getNumber(this.paginate.page, 0),
+      page: this.functionService.getNumber(this.paginate.page, 1),
       size: this.functionService.getNumber(this.paginate.size, 10),
     });
   }
